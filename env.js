297f40532346328bc7025efec90b8aaf195ab401
jai-kj <jai.j@somaiya.edu>
2022-06-14 14:27:02 +0530

    weather app done

---
+++ env.js
@@ -0,0 +1,4 @@
+const api = {
+    key: "5fabc8daac855de4c216cb010b04d157",
+    baseurl: "https://api.openweathermap.org/data/2.5",
+}
---
+++ index.js
@@ -0,0 +1,49 @@
+// capture input element into js
+const searchbox = document.querySelector(".search-box")
+
+const setQuery = e => e?.keyCode == 13 && getSearchResults(searchbox?.value)
+
+const getSearchResults = query =>
+    fetch(`${api.baseurl}/weather?q=${query}&units=metric&appid=${api.key}`)
+        .then(res => res.json())
+        .then(displayResults)
+        .catch(err => console.error(err))
+
+const displayResults = data => {
+    if (data?.cod >= "400") {
+        alert(`${searchbox?.value} Not Found!`)
+        searchbox.value = ""
+        return
+    }
+    const city = document.querySelector(".city")
+    city.innerText = `${data?.name}${
+        data?.sys?.country ? `, ${data?.sys?.country}` : ""
+    }`
+
+    const temperature = document.querySelector(".temp")
+    temperature.innerHTML = `${Math.round(data?.main?.temp)}<span>°c</span>`
+
+    const weather = document.querySelector(".weather")
+    weather.innerText = `${data?.weather[0]?.main}`
+
+    const highLow = document.querySelector(".hi-low")
+    highLow.innerText = `${Math.floor(data?.main?.temp_min)}°c / ${Math.ceil(
+        data?.main?.temp_max
+    )}°c`
+
+    searchbox.value = ""
+}
+
+// Add Current Day to HTMl
+const date = document.querySelector(".date")
+date.innerText = `${new Date()
+    .toLocaleDateString("en-us", {
+        weekday: "long",
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+    })
+    .replaceAll(/,+/gm, " ")}`
+
+// Attach event listener to input
+searchbox.addEventListener("keypress", setQuery)
